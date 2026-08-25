299e2ae19846c092e21c91bcbff86f1fbe909605
tsutsumi <tsutsumi@cse-soft.co.jp>
2026-08-25 14:27:14 +0900

    seed: 初期データとしてテスト用クーポンを追加

---
+++ sql/seed/coupons.sql
@@ -0,0 +1,15 @@
+INSERT INTO coupons (
+    code, 
+    name, 
+    discount_type, 
+    discount_value, 
+    min_order_amount, 
+    is_active
+) VALUES (
+    'WELCOME1000', 
+    '新規会員1000円引きクーポン', 
+    'amount', 
+    1000, 
+    5000, 
+    true
+);
